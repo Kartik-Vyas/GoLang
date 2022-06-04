1262dd44a67c74759474a68dd5e3edc8417da18b
Kartik-Vyas <101496656+Kartik-Vyas@users.noreply.github.com>
2022-06-04 23:32:12 +0530

    Update Variables.go
    output:-
    
    5
    9

--- Variables.go
+++ Variables.go
@@ -8,7 +8,11 @@ func main(){
 	var num1 int
 	num1 = 2
 	var num2 = 3
-	var result = num1+num2
-	fmt.Print(result)
+	num3:=4
+	num4:=5
+	var result1 = num1+num2
+	result2 := num3+num4
+	fmt.Println(result1)
+	fmt.Println(result2)
 
 }
